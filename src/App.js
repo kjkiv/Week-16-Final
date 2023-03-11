49af3ffedd26d64181f45a2160416e9700cadd70
Karl <kjkiv@yahoo.com>
2023-03-11 16:59:57 -0500

    another try

--- src/App.js
+++ src/App.js
@@ -1,48 +1,27 @@
-// App.js
-import React, { useState } from "react";
-import NavBar from "./components/NavBar";
-import MovieList from "./components/MovieList";
+import React, { useState } from 'react';
+import MovieList from './MovieList';
+import MovieForm from './MovieForm';
+import './app.css';
 
 function App() {
   const [movies, setMovies] = useState([
-    { title: "The Shawshank Redemption", rating: 9.3 },
-    { title: "The Godfather", rating: 9.2 },
-    { title: "The Godfather: Part II", rating: 9.0 },
-    { title: "The Dark Knight", rating: 9.0 },
+    { title: 'The Shawshank Redemption', poster: 'shawshank.jpg', description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.' },
+    { title: 'The Godfather', poster: 'godfather.jpg', description: 'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.' },
+    { title: 'The Dark Knight', poster: 'darkknight.jpg', description: 'When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.' }
   ]);
 
-  const addMovie = () => {
-    setMovies([
-      ...movies,
-      { title: "New Movie", rating: Math.floor(Math.random() * 10) + 1 },
-    ]);
+  const addMovie = (title, poster, description) => {
+    const newMovie = { title: title, poster: poster, description: description };
+    setMovies([...movies, newMovie]);
   };
 
   return (
     <div className="App">
-      <NavBar onAddMovie={addMovie} />
+      <h1>My Movie List</h1>
       <MovieList movies={movies} />
+      <MovieForm addMovie={addMovie} />
     </div>
   );
 }
 
 export default App;
-reportWebVitals();
-import React from "react";
-import ReactDOM from "react-dom";
-import MovieList from "./MovieList";
-
-ReactDOM.render(<MovieList />, document.getElementById("root"));
-
-import React from "react";
-import ReactDOM from "react-dom";
-import MovieList from "./MovieList";
-import Navbar from "./Navbar";
-
-ReactDOM.render(
-  <>
-    <Navbar />
-    <MovieList />
-  </>,
-  document.getElementById("root")
-);
